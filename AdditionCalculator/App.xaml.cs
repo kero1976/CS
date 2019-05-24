db9e5493591e44a9d8be9a44bccec5ccff8135a5
kero1976 <kero1976@outlook.jp>
2019-05-25 08:31:59 +0900

    空

---
+++ AdditionCalculator/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AdditionCalculator.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace AdditionCalculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
