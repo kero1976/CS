0008478c263b9da92d117a6196eea434bd27d2fd
kero1976 <kero1976@outlook.jp>
2019-05-27 23:45:12 +0900

    入力チェック機能追加中

--- AdditionCalculator/ViewModels/MainWindowViewModel.cs
+++ AdditionCalculator/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Reactive.Bindings;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reactive.Linq;
 
 namespace AdditionCalculator.ViewModels
 {
@@ -14,36 +18,45 @@ namespace AdditionCalculator.ViewModels
 
         public MainWindowViewModel()
         {
+            this.ValA = new ReactiveProperty<string>().SetValidateAttribute(() => this.ValA);
+            this.ValB = new ReactiveProperty<string>().SetValidateAttribute(() => this.ValB);
+            this.ErrMsg = new[]
+            { 
+                // エラーを束ねて 
+                this.ValA.ObserveErrorChanged,
+                this.ValB.ObserveErrorChanged
+            }
+            .CombineLatest(x =>
+            {
+                // null(エラーなし）を省いて 
+                var r = x.Where(y => y != null)
+                    // 最初のエラーを返す 
+                    .Select(y => y.OfType<string>())
+                    .FirstOrDefault(y => y.Any());
+                // 無ければ無し、エラーがあれば最初のものを返す 
+                return r == null ? "ありません" : r.FirstOrDefault();
+            })
+            .ToReactiveProperty();
 
         }
 
-        private int valA = 0;
-        public int ValA
-        {
-            get { return valA; }
-            set { SetProperty(ref valA, value); }
-        }
+        [Required(ErrorMessage = "その１ 入力してください")]
+        [RegularExpression(@"^\d{1,3}$",
+            ErrorMessage = "その１は整数3桁の範囲で入力してください。")]
+        public ReactiveProperty<string> ValA { get; set; } //= new ReactiveProperty<int>();
 
-        private int valB = 0;
-        public int ValB
-        {
-            get { return valB; }
-            set { SetProperty(ref valB, value); }
-        }
+        [Required(ErrorMessage = "その2 入力してください")]
+        public ReactiveProperty<string> ValB { get; set; } // new ReactiveProperty<int>();
+        public ReactiveProperty<int> ValC { get; set; } = new ReactiveProperty<int>();
 
-        private int valC = 0;
-        public int ValC
-        {
-            get { return valC; }
-            set { SetProperty(ref valC, value); }
-        }
+        public ReactiveProperty<string> ErrMsg { get; set; } // = new ReactiveProperty<string>();
 
         private DelegateCommand addCmd;
         public DelegateCommand AddCommand =>
             addCmd ?? (addCmd = new DelegateCommand(ExecuteAddCommand, CanExecuteAddCommand));
         void ExecuteAddCommand()
         {
-            ValC = ValA + ValB;
+            ValC.Value = int.Parse(ValA.Value) + int.Parse(ValB.Value);
         }
 
         bool CanExecuteAddCommand()
